6b4adb926285451a805df4d4f8e237883a6bcd65
stephentetley <stephen.tetley@gmail.com>
2023-10-13 20:53:00 +0100

    Implementing toEastNorth and toOSGB26.

--- doodle/gridref.py
+++ doodle/gridref.py
@@ -15,34 +15,116 @@ limitations under the License.
 
 """
 
+def toOSGB36(easting, northing): 
+    try:
+        print("A1")
+        majorChar = findMajor(easting, northing)
+        print("A2: "  + majorChar)
+        minorChar = findMinor(easting % 500000, northing % 500000)
+        print("A3: " + minorChar)
+        smallE = easting % 100000
+        print("A4: " + str(smallE))
+        smallN = northing % 100000
+        print("A5: " + str(smallN))
+        return print(f'{majorChar}{minorChar}{smallE:05}{smallN:05}')
+    except (TypeError, ValueError) as exn: 
+        print(exn)
+        return None
+
 def findMajor(e, n):
-    if   e >= 0 and e < 500_000            and n >= 0 and n < 500_000:
+    if   e >= 0       and e < 500_000   and n >= 0         and n < 500_000:
         return 'S'
-    elif e >= 500_000 and e < 1_000_000    and n >= 0 and n < 500_000:
+    elif e >= 500_000 and e < 1_000_000 and n >= 0         and n < 500_000:
         return 'T'
-    elif e >= 0 and e < 500_000            and n >= 500_000 and n < 1_000_000:
+    elif e >= 0       and e < 500_000   and n >= 500_000   and n < 1_000_000:
          return 'N'
-    elif e >= 500_000 and e < 1_000_000    and n >= 500_000 and n < 1_000_000:
+    elif e >= 500_000 and e < 1_000_000 and n >= 500_000   and n < 1_000_000:
         return 'O'
-    elif e >= 0 and e < 500_000            and n >= 1_000_000 and n < 1_500_000:
+    elif e >= 0       and e < 500_000   and n >= 1_000_000 and n < 1_500_000:
         return 'H'
-    elif e >= 500_000 and e < 1_000_000    and n >= 1_000_000 and n < 1_500_000:
+    elif e >= 500_000 and e < 1_000_000 and n >= 1_000_000 and n < 1_500_000:
         return 'J'
     else:
         None 
 
+def findMinor(e, n):
+    if   e >= 0       and e < 100_000     and n >= 0       and n < 100_000: 
+        return 'V'
+    elif e >= 100_000 and e < 200_000     and n >= 0       and n < 100_000:
+        return 'W'
+    elif e >= 200_000 and e < 300_000     and n >= 0       and n < 100_000:
+        return 'X'
+    elif e >= 300_000 and e < 400_000     and n >= 0       and n < 100_000:
+        return 'Y'
+    elif e >= 400_000 and e < 500_000     and n >= 0       and n < 100_000:
+        return 'Z'
+    elif e >= 0       and e < 100_000     and n >= 100_000 and n < 200_000:
+        return 'Q'
+    elif e >= 100_000 and e < 200_000     and n >= 100_000 and n < 200_000:
+        return 'R'
+    elif e >= 200_000 and e < 300_000     and n >= 100_000 and n < 200_000:
+        return 'S'
+    elif e >= 300_000 and e < 400_000     and n >= 100_000 and n < 200_000:
+        return 'T'
+    elif e >= 400_000 and e < 500_000     and n >= 100_000 and n < 200_000:
+        return 'U'
+    elif e >= 0       and e < 100_000     and n >= 200_000 and n < 300_000:
+        return 'L'
+    elif e >= 100_000 and e < 200_000     and n >= 200_000 and n < 300_000:
+        return 'M'
+    elif e >= 200_000 and e < 300_000     and n >= 200_000 and n < 300_000:
+        return 'N'
+    elif e >= 300_000 and e < 400_000     and n >= 200_000 and n < 300_000:
+        return 'O'
+    elif e >= 400_000 and e < 500_000     and n >= 200_000 and n < 300_000:
+        return 'P'
+    elif e >= 0       and e < 100_000     and n >= 300_000 and n < 400_000:
+        return 'F'
+    elif e >= 100_000 and e < 200_000     and n >= 300_000 and n < 400_000:
+        return 'G'
+    elif e >= 200_000 and e < 300_000     and n >= 300_000 and n < 400_000:
+        return 'H'
+    elif e >= 300_000 and e < 400_000     and n >= 300_000 and n < 400_000:
+        return 'J'
+    elif e >= 400_000 and e < 500_000     and n >= 300_000 and n < 400_000:
+        return 'K'
+    elif e >= 0       and e < 100_000     and n >= 400_000 and n < 500_000:
+        return 'A'
+    elif e >= 100_000 and e < 200_000     and n >= 400_000 and n < 500_000:
+        return 'B'
+    elif e >= 200_000 and e < 300_000     and n >= 400_000 and n < 500_000:
+        return 'C'
+    elif e >= 300_000 and e < 400_000     and n >= 400_000 and n < 500_000:
+        return 'D'
+    elif e >= 400_000 and e < 500_000     and n >= 400_000 and n < 500_000:
+        return 'E'
+    else:
+        None
+
+def toEastNorth(gridref):
+    try: 
+        major = decodeMajor(gridref[0])
+        minor = decodeMinor(gridref[1])
+        majorE, majorN = major
+        minorE, minorN = minor
+        east1 = int(gridref[2:7])
+        north1 = int(gridref[7:12])
+        return {"easting": majorE + minorE + east1, "northing": majorN + minorN + north1}
+    except (TypeError, ValueError) as exn: 
+        return None
+    
 def decodeMajor(c):
     cu = c.upper()
     if cu == 'S':
-        return (0,          0)
+        return (0,       0)
     elif cu == 'T':
-        return (500_000,    0)
+        return (500_000, 0)
     elif cu == 'N': 
-        return(0,          500_000)
+        return(0,        500_000)
     elif cu == 'O':
-        return (500_000,    500_000)
+        return (500_000, 500_000)
     elif cu == 'H':
-        return (0,          1_000_000)
+        return (0,       1_000_000)
     else:
         None
 
--- usegridref.py
+++ usegridref.py
@@ -8,4 +8,9 @@ print(decodeMajor('S'))
 print(decodeMinor('S'))
 
 print(decodeMinor('T'))
+
 print(decodeMinor('L'))
+
+print(toEastNorth("SE4563250134"))
+
+print(toOSGB36(445632, 450134))
