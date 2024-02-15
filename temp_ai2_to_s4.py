7e0689ae269031a74f0fd8ee70122e9a52ac4fd4
stephentetley <stephen.tetley@gmail.com>
2024-02-15 22:54:40 +0000

    Work on the ai2 to s4 translation experiment. Seeding the pre-pivot table with dummy EAVs that must be present as columns in the pivot table solves the translation puzzle.

--- temp_ai2_to_s4.py
+++ temp_ai2_to_s4.py
@@ -15,14 +15,40 @@ print(df)
 def rename1(s: str, t: str, df: pl.DataFrame) -> pl.DataFrame:
     return df.rename({s: t})
 
-out = df.pivot(index="sai_num", columns="attr_name", values="attr_value", aggregate_function="first")
+# NOTE
+# Best way to insure the pivot table has necessary columns is to add fake recods with them.
+# sai_num     ┆ attr_name            ┆ attr_value
+# Doing this then pivotting solves the ai2 to s4 translation puzzle
+
+sample = pl.DataFrame([ 
+    {'sai_num': 'X001', 'attr_name': 'Weight kg', 'attr_value': '10.0'},
+    {'sai_num': 'X001', 'attr_name': 'Specific Model/Frame', 'attr_value': 'Hydro1'},
+])
+
+stk = df.vstack(sample)
+out = stk.pivot(index="sai_num", columns="attr_name", values="attr_value", aggregate_function="first")
 print(rename1("Common Name", "common_name", out))
+
+
+
+
 
 # if column doesn't exist, rename throws error...
 # print(rename1("Common NameX", "common_name", out))
 
-print(out.columns)
+# def ext_serial_no(df: pl.DataFrame) -> pl.DataFrame: 
+#     if "Serial No" in df.columns:
+#         return df.rename({"Serial No": "manuf_serial_number"})
+#     else:
+#         df.with_columns(manuf_serial_number = pl.lit(""))
 
+# def ext_specific_model_frame(df: pl.DataFrame) -> pl.DataFrame: 
+#     if "Serial No" in df.columns:
+#         return df.rename({"Specific Model/Frame": "manuf_part_number"})
+#     else:
+#         return df.with_columns(manuf_part_number = pl.lit(""))
+
+print(out.columns)
 
 out1 = out.select(
     [ (pl.col("sai_num"))
@@ -30,6 +56,9 @@ out1 = out.select(
     , (pl.col("Common Name").str.extract(r"EQUIPMENT: (.+)", 1).alias("equi_type"))
     , (pl.col("Manufacturer").alias("manufacturer"))
     , (pl.col("Model").alias("model"))
+    , (pl.col("Specific Model/Frame").alias("manuf_part_no"))
+    , (pl.col("Weight kg").alias("weight"))
+    , (pl.lit("KG").alias("weight_unit"))
     , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%d.%m.%Y")).alias("startup_date")
     , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%Y")).alias("constr_year")
     , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%m")).alias("constr_month")
