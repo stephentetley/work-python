0692c9034e36eaeb271b87f038e8a53d7386f9c4
stephentetley <stephen.tetley@gmail.com>
2024-02-16 22:08:02 +0000

    Adding change missed in previous commit.

--- temp_ai2_to_s4.py
+++ temp_ai2_to_s4.py
@@ -3,6 +3,7 @@
 import polars as pl
 import polars.selectors as cs
 import duckdb
+import sptapps.ai2_to_s4.master_data as master_data
 
 con = duckdb.connect('g:/work/2024/ai2_to_s4/magflow.db', read_only=False)
 
@@ -18,53 +19,22 @@ def rename1(s: str, t: str, df: pl.DataFrame) -> pl.DataFrame:
 # NOTE
 # Best way to insure the pivot table has necessary columns is to add fake recods with them.
 # sai_num     ┆ attr_name            ┆ attr_value
-# Doing this then pivotting solves the ai2 to s4 translation puzzle
+# Doing this then pivoting solves the ai2 to s4 translation puzzle
 
-sample = pl.DataFrame([ 
-    {'sai_num': 'X001', 'attr_name': 'Weight kg', 'attr_value': '10.0'},
-    {'sai_num': 'X001', 'attr_name': 'Specific Model/Frame', 'attr_value': 'Hydro1'},
-])
 
-stk = df.vstack(sample)
+stk = df.vstack(master_data.eav_sample)
 out = stk.pivot(index="sai_num", columns="attr_name", values="attr_value", aggregate_function="first")
 print(rename1("Common Name", "common_name", out))
 
 
 
-
-
-# if column doesn't exist, rename throws error...
-# print(rename1("Common NameX", "common_name", out))
-
-# def ext_serial_no(df: pl.DataFrame) -> pl.DataFrame: 
-#     if "Serial No" in df.columns:
-#         return df.rename({"Serial No": "manuf_serial_number"})
-#     else:
-#         df.with_columns(manuf_serial_number = pl.lit(""))
-
-# def ext_specific_model_frame(df: pl.DataFrame) -> pl.DataFrame: 
-#     if "Serial No" in df.columns:
-#         return df.rename({"Specific Model/Frame": "manuf_part_number"})
-#     else:
-#         return df.with_columns(manuf_part_number = pl.lit(""))
-
 print(out.columns)
 
-out1 = out.select(
-    [ (pl.col("sai_num"))
-    , (pl.col("Common Name").str.extract(r"/([^/]+)/EQUIPMENT:", 1).alias("name"))
-    , (pl.col("Common Name").str.extract(r"EQUIPMENT: (.+)", 1).alias("equi_type"))
-    , (pl.col("Manufacturer").alias("manufacturer"))
-    , (pl.col("Model").alias("model"))
-    , (pl.col("Specific Model/Frame").alias("manuf_part_no"))
-    , (pl.col("Weight kg").alias("weight"))
-    , (pl.lit("KG").alias("weight_unit"))
-    , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%d.%m.%Y")).alias("startup_date")
-    , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%Y")).alias("constr_year")
-    , (pl.col("Installed From").str.to_datetime("%m/%d/%y %H:%M").dt.to_string("%m")).alias("constr_month")
-    ]
-)
+out1 = master_data.extract_masterdata(out)
+
 
 print(out1)
 
+con.execute('CREATE SCHEMA IF NOT EXISTS ai2_to_s4;')
+master_data.store_masterdata(con=con, exec_ddl=True, df=out1)
 
