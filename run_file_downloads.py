cb9fef2eda0ee1a29ab3f0d51bc8fdd593328a9d
stephentetley <stephen.tetley@gmail.com>
2024-03-20 22:05:50 +0000

    Changed file_download parser to use Polars.

--- run_file_downloads.py
+++ run_file_downloads.py
@@ -3,23 +3,16 @@
 import os
 from sptlibs.file_download.gen_duckdb import GenDuckdb
 
-source_directories = ['g:/work/2023/file_download/test01']
-glob_pattern = '*download.txt'
-output_directory = 'g:/work/2023/file_download/test01'
-classlists_db = 'g:/work/2023/classlist/classlists.duckdb'
+source_directory = 'g:/work/2024/file_download/valua_downloads'
+glob_pattern = '*valua*.txt'
+output_path = 'g:/work/2024/file_download/valua_downloads/valua.duckdb'
+classlists_db = 'g:/work/2024/classlists/classlists.duckdb'
 
 
 
-if os.path.exists(output_directory): 
-    print(f'Found: {output_directory}')
-
-    genduckdb = GenDuckdb()
-    genduckdb.set_output_directory(output_directory=output_directory)
-    for dir in source_directories: 
-        genduckdb.add_file_downloads_in_directory(path=dir, glob_pattern=glob_pattern)
-    genduckdb.db_name = 'test01.duckdb'
-    genduckdb.add_classlist_tables(classlists_duckdb_path=classlists_db)
-    genduckdb.gen_duckdb()
+genduckdb = GenDuckdb(classlists_duckdb_path=classlists_db, duckdb_output_path=output_path)
+genduckdb.add_downloads_source_directory(source_dir=source_directory, glob_pattern=glob_pattern)
+genduckdb.gen_duckdb()
 
 # Note - file downloads best used for checking so we need most of the information in equi downloads.
 # Still need to know char_type to choose which field to look at for value in the valuaequi field.
--- sptlibs/file_download/load_file_download.py
+++ sptlibs/file_download/load_file_download.py
@@ -17,8 +17,8 @@ limitations under the License.
 
 import re
 import duckdb
-import pandas as pd
-import sptlibs.import_utils as import_utils
+import polars as pl
+import sptlibs.polars_import_utils as polars_import_utils
 
 
 def load_file_download(*, path: str, con: duckdb.DuckDBPyConnection) -> None:
@@ -47,10 +47,10 @@ def load_file_download(*, path: str, con: duckdb.DuckDBPyConnection) -> None:
                 if len(row) > 0: 
                     _tidy_row(row)
                     rows.append(row)
-    table_name1 = import_utils.normalize_name(f'{entity_type}_{variant}')
+    table_name1 = polars_import_utils.normalize_name(f'{entity_type}_{variant}')
     qualified_table_name = f's4_fd_raw_data.{table_name1}'
-    df = pd.DataFrame(rows, columns = columns)
-    df_renamed = import_utils.normalize_df_column_names(df)
+    df = pl.DataFrame(data=rows, schema=columns)
+    df_renamed = polars_import_utils.normalize_df_column_names(df)
     vw_df = f'vw_df_{table_name1}'
     con.register(view_name=vw_df, python_object=df_renamed)
     sql_stmt = f'CREATE OR REPLACE TABLE {qualified_table_name} AS SELECT * FROM {vw_df};'
