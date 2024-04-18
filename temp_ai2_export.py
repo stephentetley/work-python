62cb6ef7bb5242d29152c4e6c8aad464dfc950b7
stephentetley <stephen.tetley@gmail.com>
2024-04-18 23:02:34 +0100

    Working towards unpivoting ai2 EAV table to make asset classes.

--- temp_ai2_export.py
+++ temp_ai2_export.py
@@ -32,5 +32,59 @@ ai2_export_to_ai2_class_rep.ai2_export_to_ai2_classes(con=conn)
 cr_gen_table.gen_cr_table(pk_name='equi_id', schema_name='ai2_class_rep', class_name='LSTNUT', con=conn)
 cr_gen_table.gen_cr_table(pk_name='equi_id', schema_name='ai2_class_rep', class_name='FSTNEM', con=conn)
 
+####
+
+columns_query = """
+    SELECT 
+        lower(ec.char_name) AS column_name
+    FROM s4_classlists.equi_characteristics ec
+    WHERE 
+        ec.class_name = 'LSTNUT'
+    ;
+"""
+lstnut_columns = conn.execute(columns_query).pl().get_column('column_name').to_list()
+
+print("lstnut_columns: {}".format(lstnut_columns))
+
+
+lstnut_pivot = """
+    SELECT 
+        md.ai2_reference AS ai2_reference, 
+        pv.* EXCLUDE (ai2_reference)
+    FROM
+        ai2_export.master_data md
+    JOIN (PIVOT ai2_export.eav_data ON attribute_name USING first(attribute_value) GROUP BY ai2_reference) pv ON pv.ai2_reference = md.ai2_reference 
+    WHERE 
+        md.common_name LIKE '%EQUIPMENT: ULTRASONIC LEVEL INSTRUMENT'
+    """
+
+df = conn.execute(lstnut_pivot).pl()
+print(df)
+
+source_columns = df.columns
+print("source_columns: {}".format(source_columns))
+
+
+existing_columns = [x for x in lstnut_columns if x in source_columns]
+missing_columns = [x for x in lstnut_columns if not x in source_columns]
+
+
+## initialize by copying existing columns:
+print("Existing columns:{}".format(existing_columns))
+data = {x: df.get_column(x) for x in existing_columns}
+data["ai2_reference"] = df.get_column("ai2_reference")
+
+df2 = pl.DataFrame(
+    data
+)
+print(df2)
+
+## add columns missing from `df` because they had no data:
+df2 = df2.with_columns(
+    [pl.lit("").alias(x) for x in missing_columns]
+)
+
+print(df2)
+
 conn.close()
 print("done")
