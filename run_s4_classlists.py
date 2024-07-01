72c556aa4d06c8f4babd72c4510c6209d1740261
stephentetley <stephen.tetley@gmail.com>
2024-07-01 17:47:29 +0100

    Removing old apps from sptapps.

--- run_s4_classlists.py
+++ run_s4_classlists.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
+
 from argparse import ArgumentParser
 import duckdb
 from sptlibs.utils.asset_data_config import AssetDataConfig
