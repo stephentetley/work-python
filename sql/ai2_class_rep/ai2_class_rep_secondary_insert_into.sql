d3d9bc915f72c5ace02ea6cca0596b8ed9dbe1eb
stephentetley <stephen.tetley@gmail.com>
2024-07-03 11:49:44 +0100

    Removed temp. ai2_class_rep - added outstation record to podeup translation.

--- sql/ai2_class_rep/ai2_class_rep_secondary_insert_into.sql
+++ sql/ai2_class_rep/ai2_class_rep_secondary_insert_into.sql
@@ -14,19 +14,19 @@
 -- limitations under the License.
 -- 
 
--- ## NETWMO (Network Device connecting to Telecoms)
+-- ## NETWMO (Network Device connecting to Telecoms) / from EQUIPMENT: TELEMETRY OUTSTATION
 
 --- source table may have _duplicates_ hence use `DISTINCT ON`...
 INSERT INTO ai2_class_rep.equi_master_data BY NAME
 SELECT DISTINCT ON (emd.ai2_reference)
-    hash(emd.ai2_reference || '_modem') AS equipment_key,
+    hash(emd.ai2_reference || '_outstation_modem') AS equipment_key,
     emd.ai2_reference AS ai2_reference,
     emd.common_name AS common_name,
     regexp_extract(emd.common_name, '/([^/]*)/EQUIPMENT:', 1) || ' (modem)' AS equipment_name,
-    '!!MODEM' AS equipment_type,
-    -- emd.installed_from AS installed_from,
+    '!!OUTSTATION MODEM' AS equipment_type,
+    any_value(CASE WHEN eav.attribute_name = 'modem_install_date' THEN TRY_CAST(try_strptime(eav.attribute_value, '%b %-d %Y') AS DATE) ELSE NULL END) AS installed_from,
     emd.asset_status AS asset_status,
-    any_value(CASE WHEN eav.attribute_name = 'modem_manufacturer' THEN eav.attribute_value ELSE NULL END) AS  manufacturer,
+    any_value(CASE WHEN eav.attribute_name = 'modem_manufacturer' THEN eav.attribute_value ELSE NULL END) AS manufacturer,
     any_value(CASE WHEN eav.attribute_name = 'modem_type' THEN eav.attribute_value ELSE NULL END) AS model,
     any_value(CASE WHEN eav.attribute_name = 'modem_serial_number' THEN eav.attribute_value ELSE NULL END) AS serial_number,
     any_value(CASE WHEN eav.attribute_name = 'work_centre' THEN eav.attribute_value ELSE NULL END) AS work_centre,
@@ -40,7 +40,7 @@ GROUP BY emd.ai2_reference, emd.common_name, emd.asset_status;
 -- uses scalar udfs `udf_get_easting` and `udf_get_northing` that must be registered first...
 INSERT INTO ai2_class_rep.equi_east_north BY NAME
 SELECT DISTINCT ON(emd.ai2_reference)
-    hash(emd.ai2_reference || '_modem') AS equipment_key,
+    hash(emd.ai2_reference || '_outstation_modem') AS equipment_key,
     emd.ai2_reference AS ai2_reference,
     any_value(CASE WHEN eav.attribute_name = 'loc_ref' THEN eav.attribute_value ELSE NULL END) AS grid_ref,
     any_value(CASE WHEN eav.attribute_name = 'loc_ref' THEN udf_get_easting(eav.attribute_value) ELSE NULL END) AS easting,
@@ -54,7 +54,58 @@ GROUP BY emd.ai2_reference;
 INSERT OR REPLACE INTO ai2_class_rep.equiclass_netwmo BY NAME
 WITH cte AS(
     SELECT DISTINCT ON(emd.ai2_reference)
-        hash(emd.ai2_reference || '_modem') AS equipment_key,
+        hash(emd.ai2_reference || '_outstation_modem') AS equipment_key,
+        emd.ai2_reference AS ai2_reference, 
+        any_value(CASE WHEN eav.attribute_name = 'location_on_site' THEN eav.attribute_value ELSE NULL END) AS location_on_site,
+    FROM ai2_export.equi_master_data emd
+    JOIN ai2_export.equi_eav_data eav ON eav.ai2_reference = emd.ai2_reference 
+    WHERE emd.common_name LIKE '%EQUIPMENT: TELEMETRY OUTSTATION'
+    GROUP BY emd.ai2_reference)
+SELECT 
+    COLUMNS(c -> NOT starts_with(c, '__'))
+FROM cte;
+
+-- ## PODETU (Network Device connecting to Telecoms) / from EQUIPMENT: TELEMETRY OUTSTATION
+
+--- source table may have _duplicates_ hence use `DISTINCT ON`...
+INSERT INTO ai2_class_rep.equi_master_data BY NAME
+SELECT DISTINCT ON (emd.ai2_reference)
+    hash(emd.ai2_reference || '_outstation_psu') AS equipment_key,
+    emd.ai2_reference AS ai2_reference,
+    emd.common_name AS common_name,
+    regexp_extract(emd.common_name, '/([^/]*)/EQUIPMENT:', 1) || ' (modem)' AS equipment_name,
+    '!!OUTSTATION PSU' AS equipment_type,
+    any_value(CASE WHEN eav.attribute_name = 'psu_install_date' THEN TRY_CAST(try_strptime(eav.attribute_value, '%b %-d %Y') AS DATE) ELSE NULL END) AS installed_from,
+    emd.asset_status AS asset_status,
+    any_value(CASE WHEN eav.attribute_name = 'psu_manufacturer' THEN eav.attribute_value ELSE NULL END) AS manufacturer,
+    any_value(CASE WHEN eav.attribute_name = 'psu_type' THEN eav.attribute_value ELSE NULL END) AS model,
+    any_value(CASE WHEN eav.attribute_name = 'psu_serial_number' THEN eav.attribute_value ELSE NULL END) AS serial_number,
+    any_value(CASE WHEN eav.attribute_name = 'work_centre' THEN eav.attribute_value ELSE NULL END) AS work_centre,
+    any_value(CASE WHEN eav.attribute_name = 'responsible_officer' THEN eav.attribute_value ELSE NULL END) AS responsible_officer,
+FROM ai2_export.equi_master_data emd
+JOIN ai2_export.equi_eav_data eav ON eav.ai2_reference = emd.ai2_reference
+WHERE emd.common_name LIKE '%EQUIPMENT: TELEMETRY OUTSTATION'
+GROUP BY emd.ai2_reference, emd.common_name, emd.asset_status;
+
+
+-- uses scalar udfs `udf_get_easting` and `udf_get_northing` that must be registered first...
+INSERT INTO ai2_class_rep.equi_east_north BY NAME
+SELECT DISTINCT ON(emd.ai2_reference)
+    hash(emd.ai2_reference || '_outstation_psu') AS equipment_key,
+    emd.ai2_reference AS ai2_reference,
+    any_value(CASE WHEN eav.attribute_name = 'loc_ref' THEN eav.attribute_value ELSE NULL END) AS grid_ref,
+    any_value(CASE WHEN eav.attribute_name = 'loc_ref' THEN udf_get_easting(eav.attribute_value) ELSE NULL END) AS easting,
+    any_value(CASE WHEN eav.attribute_name = 'loc_ref' THEN udf_get_northing(eav.attribute_value) ELSE NULL END) AS northing,
+FROM ai2_export.equi_master_data emd
+JOIN ai2_export.equi_eav_data eav ON eav.ai2_reference = emd.ai2_reference 
+WHERE emd.common_name LIKE '%EQUIPMENT: TELEMETRY OUTSTATION'
+GROUP BY emd.ai2_reference;
+
+
+INSERT OR REPLACE INTO ai2_class_rep.equiclass_podetu BY NAME
+WITH cte AS(
+    SELECT DISTINCT ON(emd.ai2_reference)
+        hash(emd.ai2_reference || '_outstation_psu') AS equipment_key,
         emd.ai2_reference AS ai2_reference, 
         any_value(CASE WHEN eav.attribute_name = 'location_on_site' THEN eav.attribute_value ELSE NULL END) AS location_on_site,
     FROM ai2_export.equi_master_data emd
