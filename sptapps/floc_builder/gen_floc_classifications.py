ae3545c432f11d5768a70e2260a7a6471779787a
stephentetley <stephen.tetley@gmail.com>
2024-12-08 21:50:33 +0000

    floc_builder - more work on generating classifications.

--- sptapps/floc_builder/gen_floc_classifications.py
+++ sptapps/floc_builder/gen_floc_classifications.py
@@ -27,7 +27,9 @@ class GenFlocClassifications:
     def gen_floc_classifications(self) -> list[FlocClassification]:
         for x in self.flocs:
             self._add_aib_reference(x)
+            self._add_east_north(x)
             self._add_sys_classification(x)
+            self._add_uniclass(x)
 
         ls = list(self.chars)
         ls.sort(key=lambda x: f'{x.functional_location.ljust(30, "-")}{x.class_name}')
@@ -40,75 +42,39 @@ class GenFlocClassifications:
                                   characteristic_value='')
         self.chars.add(char)
         return self
+
+    def _add_east_north(self, floc: Floc) -> Self:
+        easting = FlocClassification(functional_location=floc.functional_location,
+                                     class_name='EAST_NORTH', 
+                                     characteristic_name='EASTING',
+                                     characteristic_value='0')
+        self.chars.add(easting)
+        northing = FlocClassification(functional_location=floc.functional_location,
+                                      class_name='EAST_NORTH', 
+                                      characteristic_name='NORTHING',
+                                      characteristic_value='0')
+        self.chars.add(northing)
+        return self
     
     def _add_sys_classification(self, floc: Floc) -> Self:
         if floc.category == 5 and floc.class_type:
             char = FlocClassification(functional_location=floc.functional_location,
                                       class_name=floc.class_type, 
                                       characteristic_name='SYSTEM_TYPE',
-                                      characteristic_value='TODO'
-                                      )
+                                      characteristic_value='TODO')
             self.chars.add(char)
         return self
 
-    # def _add_function_floc(self, sys: System) -> Self:
-    #     otype = sys.path[0:3]
-    #     descr = self.get_description(otype)
-    #     floc = Floc(functional_location=f'{self.site.siteid}-{otype}',
-    #                 description=descr,
-    #                 startup_date=self.startup_date,
-    #                 object_type=otype,
-    #                 structure_indicator='YW-GS', 
-    #                 status='OPER')
-    #     self.flocs.add(floc)
-    #     return self
-    
-    # def _add_process_group_floc(self, sys: System) -> Self:
-    #     otype = sys.path[4:7]
-    #     descr = self.get_description(otype)
-    #     floc = Floc(functional_location=f'{self.site.siteid}-{sys.path[0:7]}',
-    #                 description=descr,
-    #                 startup_date=self.startup_date,
-    #                 object_type=otype,
-    #                 structure_indicator='YW-GS', 
-    #                 status='OPER')
-    #     self.flocs.add(floc)
-    #     return self
-
-    # def _add_process_floc(self, sys: System) -> Self:
-    #     otype = sys.path[8:11]
-    #     descr = self.get_description(otype)
-    #     floc = Floc(functional_location=f'{self.site.siteid}-{sys.path}',
-    #                 description=descr,
-    #                 startup_date=self.startup_date,
-    #                 object_type=otype,
-    #                 structure_indicator='YW-GS', 
-    #                 status='OPER')
-    #     self.flocs.add(floc)
-    #     return self
-    
-    # def _add_sys_floc(self, sys: System) -> Self:
-    #     floc = Floc(functional_location=f'{self.site.siteid}-{sys.key}',
-    #                 description=sys.name,
-    #                 startup_date=self.startup_date,
-    #                 object_type=sys.otype,
-    #                 structure_indicator='YW-GS', 
-    #                 status='OPER', 
-    #                 class_type=sys.ctype)
-    #     self.flocs.add(floc)
-    #     return self
-        
-    # def _add_subsystem_flocs(self, sys:System) -> Self:
-    #     for ss in sys.subsystems.values():
-    #         self._add_subsystem_floc(ss, sys.key)
-
-    # def _add_subsystem_floc(self, subsys:Subsystem, path: str) -> Self:
-    #     floc = Floc(functional_location=f'{self.site.siteid}-{path}-{subsys.key}',
-    #                 description=subsys.name,
-    #                 object_type=subsys.otype,
-    #                 startup_date=self.startup_date,
-    #                 structure_indicator='YW-GS', 
-    #                 status='OPER')
-    #     self.flocs.add(floc)
-    #     return self
-
+    def _add_uniclass(self, floc: Floc) -> Self:
+        if floc.category != 5:
+            ucode = FlocClassification(functional_location=floc.functional_location,
+                                       class_name='UNICLASS_CODE', 
+                                       characteristic_name='UNICLASS_CODE',
+                                       characteristic_value='')
+            self.chars.add(ucode)
+            udesc = FlocClassification(functional_location=floc.functional_location,
+                                       class_name='UNICLASS_CODE', 
+                                       characteristic_name='UNICLASS_DESC',
+                                       characteristic_value='')
+            self.chars.add(udesc)
+        return self
