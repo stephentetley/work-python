c6e8f59468d10c63446475d652fcee2654fcc686
stephentetley <stephen.tetley@gmail.com>
2025-01-12 10:29:47 +0000

    Work on SQL for floc_builder

--- sql/floc_builder/floc_builder.sql
+++ sql/floc_builder/floc_builder.sql
@@ -1,5 +1,31 @@
--- TODO - only precede 1-4 levels for level 5, level 6 only generate level 6
+-- 
+-- Copyright 2025 Stephen Tetley
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
 
+CREATE SCHEMA IF NOT EXISTS working;
+
+CREATE OR REPLACE TABLE working.existing_flocs AS
+SELECT 
+    t.functional_location AS funcloc,
+    t.description_of_functional_location AS floc_name,
+FROM raw_data.ih06_export t
+ORDER BY funcloc
+;
+    
+
+CREATE OR REPLACE TABLE working.gen_flocs AS  
 WITH 
 cte1 AS (
     SELECT 
@@ -18,10 +44,8 @@ cte1 AS (
         IF (func        IS NOT NULL, concat_ws('-', site, func), NULL) AS level2,
         IF (proc_grp    IS NOT NULL, concat_ws('-', site, func, proc_grp), NULL) AS level3,
         IF (proc        IS NOT NULL, concat_ws('-', site, func, proc_grp, proc), NULL) AS level4,
-        -- TODO - generate either level 5 or level 6 depending on floc size
-        
-        IF (floc_category = 5, concat_ws('-', site, func, proc_grp, proc, sysm), NULL) AS level5,
-        IF (floc_category = 6, concat_ws('-', site, func, proc_grp, proc, sysm, subsysm), NULL) AS level6,
+        IF (sysm        IS NOT NULL, concat_ws('-', site, func, proc_grp, proc, sysm), NULL) AS level5,
+        IF (floc_category = 6,       concat_ws('-', site, func, proc_grp, proc, sysm, subsysm), NULL) AS level6,
     FROM raw_data.worklist t
     ),
     cte2 AS (
@@ -34,53 +58,65 @@ cte1 AS (
         LEFT JOIN s4_ztables.flocdes t2 ON t2.objtype = t1.func
         LEFT JOIN s4_ztables.flocdes t3 ON t3.objtype = t1.proc_grp
         LEFT JOIN s4_ztables.flocdes t4 ON t4.objtype = t1.proc
-    )
+    )  
 (SELECT  
-    site AS floc,  
+    site AS funcloc,
     source_name AS name,
     1 AS floc_category,
     'SITE' AS floc_type,
     NULL AS floc_class,
+    NULL AS parent_floc,
 FROM cte2 WHERE cte2.floc_category = 1)
 UNION BY NAME
 (SELECT 
-    level2 AS floc,
+    level2 AS funcloc,
     name_2 AS name,
     2 AS floc_category,
     func AS floc_type,
     NULL AS floc_class,
+    site AS parent_floc,
 FROM cte2 WHERE cte2.level2 IS NOT NULL)
 UNION BY NAME 
 (SELECT 
-    level3 AS floc, 
+    level3 AS funcloc,
     name_3 AS name,
     3 AS floc_category,
     proc_grp AS floc_type,
     NULL AS floc_class,
+    level2 AS parent_floc,
 FROM cte2 WHERE cte2.level3 IS NOT NULL)
 UNION BY NAME
 (SELECT 
-    level4 AS floc,
+    level4 AS funcloc,
     name_4 AS name,
     4 AS floc_category,
     proc AS floc_type,
     NULL AS floc_class,
+    level3 AS parent_floc,
 FROM cte2 WHERE cte2.level4 IS NOT NULL)
 UNION BY NAME
 (SELECT 
-    level5 AS floc, 
+    level5 AS funcloc, 
     source_name AS name, 
     5 AS floc_category,
     source_class_type AS floc_class,
     source_type AS floc_type, 
+    level4 AS parent_floc,
 FROM cte2 WHERE cte2.floc_category = 5)
 UNION BY NAME
 (SELECT 
-    level6 AS floc, 
+    level6 AS funcloc, 
     source_name AS name,
     6 AS floc_category,
     source_type AS floc_type, 
     NULL AS floc_class,
+    level5 AS parent_floc,
 FROM cte2 WHERE cte2.floc_category = 6)
-ORDER BY floc
+ORDER BY funcloc
+;
+
+SELECT t1.* 
+FROM working.gen_flocs t1
+ANTI JOIN working.existing_flocs t2 ON t2.funcloc = t1.funcloc
+ORDER BY funcloc
 ;
