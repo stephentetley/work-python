1a43fb17143b3097954ed39c4d2d7b822c6c3be9
stephentetley <stephen.tetley@gmail.com>
2025-01-26 16:00:25 +0000

    Added first equiclass translation - stardo

--- src/sptapps/equi_asset_translation/equi_asset_translation.py
+++ src/sptapps/equi_asset_translation/equi_asset_translation.py
@@ -71,5 +71,7 @@ def translate_ai2_eav_to_ai2_classrep(*, con: duckdb.DuckDBPyConnection) -> None
 def translate_ai2_classrep_to_s4_classrep(*, con: duckdb.DuckDBPyConnection) -> None:
     runner = SqlScriptRunner2(__file__, con=con)
     runner.exec_sql_file(rel_file_path='ai2_classrep_to_s4_classrep/setup_equi_asset_translation.sql')
+    runner.exec_sql_file(rel_file_path='ai2_classrep_to_s4_classrep/setup_equi_asset_translation_macros.sql')
+    runner.exec_sql_file(rel_file_path='ai2_classrep_to_s4_classrep/translate_equiclass_electrical.sql')
     runner.exec_sql_file(rel_file_path='ai2_classrep_to_s4_classrep/s4_classrep_equi_masterdata_insert_into.sql')
 
---
+++ src/sptapps/equi_asset_translation/sql/ai2_classrep_to_s4_classrep/translate_equiclass_electrical.sql
@@ -0,0 +1,37 @@
+-- 
+-- Copyright 2025 Stephen Tetley
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+
+
+
+
+INSERT OR REPLACE INTO s4_classrep.equiclass_stardo BY NAME
+WITH cte AS (
+SELECT
+    t.equipment_id AS equipment_id,  
+    t._ip_rating AS ip_rating,
+    t._location_on_site AS location_on_site,
+    -- star_rated_current
+    TRY_CAST(t._current_in AS DECIMAL) AS star_rated_current_a,
+    -- star_rated_power_kw
+    TRY_CAST(t._power AS DECIMAL) AS __dec_power,
+    equi_asset_translation.power_to_killowatts(t._power_units, __dec_power) AS star_rated_power_kw,
+    -- star_rated_voltage / star_rated_voltage_units
+    equi_asset_translation.voltage_ac_or_dc(t._voltage_in_ac_or_dc) AS star_rated_voltage_units,
+    TRY_CAST(t._voltage_in AS INTEGER) AS star_rated_voltage,
+FROM ai2_classrep.equiclass_direct_on_line_starter t
+)
+SELECT COLUMNS(c -> c NOT LIKE '$_$_%' ESCAPE '$') FROM cte
+;
