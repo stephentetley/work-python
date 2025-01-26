ae6c42de29827869663bf392021c7451ac8e0373
stephentetley <stephen.tetley@gmail.com>
2025-01-26 10:34:12 +0000

    Fixed errors in temp_equi_translation.py

--- scripts/temp_equi_translation.py
+++ scripts/temp_equi_translation.py
@@ -1,7 +1,7 @@
 
 import duckdb
 from sptlibs.utils.xlsx_source import XlsxSource
-import sptapps.equi_asset_translation.equi_translation as equi_translation
+import sptapps.equi_asset_translation.equi_asset_translation as equi_asset_translation
 
 ## (base) > $env:PYTHONPATH='E:\coding\work\work-python\src'
 ## (base) > python.exe .\scripts\temp_equi_translation.py
@@ -16,16 +16,17 @@ duckdb_output_path  = 'g:/work/2025/equi_translation/mal12_new_equi_translation.
 
 con = duckdb.connect(database=duckdb_output_path, read_only=False)
 
-equi_translation.setup_equi_translation(con=con, 
+equi_asset_translation.setup_equi_translation(con=con, 
                                               s4_classlists_db_source=s4_classlists_source,
                                               ai2_equipment_attributes_source=ai2_equipment_attributes_source)
 
 # load ai2 exports into landing area...
-equi_translation.import_ai2_exports_to_ai2_landing(con=con, 
+equi_asset_translation.import_ai2_exports_to_ai2_landing(con=con, 
                                                          source_folder=source_folder,
                                                          glob_pattern='mal12-ai2*.xlsx')
 
-equi_translation.translate_ai2_eav_to_ai2_classrep(con=con)
+equi_asset_translation.ai2_landing_data_to_ai2_eav(con=con)
+equi_asset_translation.translate_ai2_eav_to_ai2_classrep(con=con)
 
 con.close()
 print(f'wrote {duckdb_output_path}')
