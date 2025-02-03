2901798fd08c25c20423ee206d32d0082e36c6ca
stephentetley <stephen.tetley@gmail.com>
2025-02-03 21:41:48 +0000

    Adding Sql stubs to translate_equiclass_d_to_f.sql

--- src/sptapps/equi_asset_translation/sql/ai2_classrep_to_s4_classrep/translate_equiclass_d_to_f.sql
+++ src/sptapps/equi_asset_translation/sql/ai2_classrep_to_s4_classrep/translate_equiclass_d_to_f.sql
@@ -14,42 +14,123 @@
 -- limitations under the License.
 -- 
 
--- # DAMSCO Dam Core
+-- # DAMSCO Dam Core (no ai2 match)
+
 -- # DAMSCR Dam Crest
--- # DAMSCT Cut off Trench
+INSERT OR REPLACE INTO s4_classrep.equiclass_damscr BY NAME
+SELECT
+    t.equipment_id AS equipment_id,
+FROM ai2_classrep.equiclass_dam_crest t;
+
+-- # DAMSCT Cut off Trench (no ai2 match)
+
 -- # DAMSDM Dam
--- # DAMSFD Dam Face Downstream
--- # DAMSFP Dam Face Upstream
--- # DAMSMISC Miscellaneous Dam Structure
+INSERT OR REPLACE INTO s4_classrep.equiclass_damsdm BY NAME
+SELECT
+    t.equipment_id AS equipment_id,
+FROM ai2_classrep.equiclass_dam t;
+
+-- # DAMSFD Dam Face Downstream (no ai2 match)
+
+-- # DAMSFP Dam Face Upstream (no ai2 match)
+
 -- # DAMSSB Stilling Basin
+INSERT OR REPLACE INTO s4_classrep.equiclass_damssb BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_stilling_basin t;
+
 -- # DAMSTB Tumble Bay
+INSERT OR REPLACE INTO s4_classrep.equiclass_damstb BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_tumble_bay t;
+
 -- # DAMSTU Tunnel
+INSERT OR REPLACE INTO s4_classrep.equiclass_damstu BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_tunnel t;
+
 -- # DAMSWW Wave Wall
+INSERT OR REPLACE INTO s4_classrep.equiclass_damsww BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_wave_wall t;
+
 -- # DECOEB Emergency Eye Wash Station
+INSERT OR REPLACE INTO s4_classrep.equiclass_decoeb BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+    t._location_on_site AS location_on_site,
+FROM ai2_classrep.equiclass_emergency_eye_bath t;
+
 -- # DECOES Emergency Eye Wash and Shower
--- # DECOMISC Miscellaneous Decontamination Unit
+INSERT OR REPLACE INTO s4_classrep.equiclass_decoes BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+    t._location_on_site AS location_on_site,
+FROM ai2_classrep.equiclass_emergency_shower_eye_bath t;
+
 -- # DECOSH Emergency Shower
--- # DECTEX Gas Detector for Explosive Atmospheres
--- # DECTFM Detector for boiler flame sources
--- # DECTMISC Miscellaneous Detection Device
--- # DECTPA Monitor Panel for Gas Detectors
--- # DECTTX Gas Detector for Toxic Atmospheres
--- # DESTOZ Ozone Destructor
--- # DHUMAC Air Conditioning Dehumidifier
--- # DHUMDE Desiccant Dehumidifier
--- # DHUMMISC Miscellaneous Dehumidifier
+INSERT OR REPLACE INTO s4_classrep.equiclass_decosh BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+    t._location_on_site AS location_on_site,
+FROM ai2_classrep.equiclass_emergency_shower t;
+
+-- # DECTEX Gas Detector for Explosive Atmospheres (no ai2 match)
+-- # DECTFM Detector for boiler flame sources (no ai2 match)
+-- # DECTPA Monitor Panel for Gas Detectors (no ai2 match)
+-- # DECTTX Gas Detector for Toxic Atmospheres (no ai2 match)
+-- # DESTOZ Ozone Destructor (no ai2 match)
+-- # DHUMAC Air Conditioning Dehumidifier (no ai2 match)
+-- # DHUMDE Desiccant Dehumidifier (no ai2 match)
+
 -- # DISTBD Distribution Board
--- # DMONAP Alignment Pins
--- # DMONCM Crack Monitoring Point
+INSERT OR REPLACE INTO s4_classrep.equiclass_distbd BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+    t._location_on_site AS location_on_site,
+FROM ai2_classrep.equiclass_distribution_board t;
+
+-- # DMONAP Alignment Pins (no ai2 match)
+-- # DMONCM Crack Monitoring Point (no ai2 match)
+
 -- # DMONEX Extensometer
+INSERT OR REPLACE INTO s4_classrep.equiclass_dmonex BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_extensometer t;
+
 -- # DMONHP Hanging Plum Bob
+INSERT OR REPLACE INTO s4_classrep.equiclass_dmonhp BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_hanging_plum_bob t;
+
 -- # DMONIN Inclinometer
+INSERT OR REPLACE INTO s4_classrep.equiclass_dmonin BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_inclinometer t;
+
 -- # DMONIP Inverted Plum Bob
--- # DMONLP Levelling Pins
--- # DMONMISC Miscellaneous Deformation Monitor
--- # DMONSC Survey Control Point
--- # DMONSP Survey Points
+INSERT OR REPLACE INTO s4_classrep.equiclass_dmonip BY NAME
+SELECT
+    t.equipment_id AS equipment_id, 
+FROM ai2_classrep.equiclass_inverted_plum_bob t;
+
+-- # DMONLP Levelling Pins (no ai2 match)
+-- # DMONSC Survey Control Point (no ai2 match)
+-- # DMONSP Survey Points (no ai2 match)
+
 -- # DRANDR Drain
+INSERT OR REPLACE INTO s4_classrep.equiclass_drandr BY NAME
+SELECT
+    t.equipment_id AS equipment_id,  
+FROM ai2_classrep.equiclass_drain t;
+
 -- # DRANMISC Miscellaneous Drain
 -- # DRANMT Mitre Drain
 -- # DRANPR Pressure Relief Well
