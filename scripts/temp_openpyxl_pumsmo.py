c55cfe1c3d6ea4b307c0e06ca3aa8bc7f3ab35a6
stephentetley <stephen.tetley@gmail.com>
2025-03-29 14:20:29 +0000

    Using defined names rather than tables for validation formulas to get a dependent drop down

--- scripts/temp_openpyxl_pumsmo.py
+++ scripts/temp_openpyxl_pumsmo.py
@@ -1,8 +1,13 @@
 from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.workbook.defined_name import DefinedName
 from openpyxl.worksheet.table import Table
 from openpyxl.utils import get_column_letter
+from openpyxl.utils import quote_sheetname, absolute_coordinate
+
+# Write directly with openpyxl
+# Trying to write from Pandas gives away control over validation
 
 # Ideally column_index and ws_meta would be encapsulated state
 def add_enums_table(*, 
@@ -40,10 +45,12 @@ add_enums_table(table_name='TABLE1',
                               ],
                 ws_meta=md)
 
+ref =  f"{quote_sheetname(md.title)}!{absolute_coordinate('A2:A7')}"
+defn = DefinedName("insulation_class_range", attr_text=ref)
+wb.defined_names.add(defn)
 
 ws.cell(row=1, column=1).value = 'Insulation Class'
-
-dv = DataValidation(type="list", formula1='INDIRECT("TABLE1[INSULATION_CLASS_DEG_C]")', allow_blank=True)
+dv = DataValidation(type="list", formula1='insulation_class_range', allow_blank=True)
 ws.add_data_validation(dv)
 dv.add('A2:A1000')
 
@@ -58,10 +65,79 @@ add_enums_table(table_name='TABLE2',
                 ws_meta=md)
 
 ws.cell(row=1, column=2).value = 'Lifting Type'
-
 dv = DataValidation(type="list", formula1='INDIRECT("TABLE2[PUMS_LIFTING_TYPE]")', allow_blank=True)
 ws.add_data_validation(dv)
 dv.add('B2:B1000')
 
+# Use Defined names rather than tables, see
+# https://openpyxl.readthedocs.io/en/stable/defined_names.html
 
-wb.save("pumsmo.xlsx")
+
+add_enums_table(table_name='TABLE3',
+                column_header='CATEGORY',
+                column_index=3,
+                enum_values= ['E',
+                              'H',
+                              'I'
+                              ],
+                ws_meta=md)
+
+ref =  f"{quote_sheetname(md.title)}!{absolute_coordinate('C2:C5')}"
+defn = DefinedName("category_range", attr_text=ref)
+wb.defined_names.add(defn)
+
+add_enums_table(table_name='TABLE4',
+                column_header='E_OBJTYPE',
+                column_index=4,
+                enum_values= ['POGE',
+                              'PODE',
+                              'METER'
+                              ],
+                ws_meta=md)
+
+ref =  f"{quote_sheetname(md.title)}!{absolute_coordinate('D2:D5')}"
+defn = DefinedName("E_OBJTYPE", attr_text=ref)
+wb.defined_names.add(defn)
+
+
+add_enums_table(table_name='TABLE5',
+                column_header='H_OBJTYPE',
+                column_index=5,
+                enum_values= ['LDSS'
+                              ],
+                ws_meta=md)
+
+ref =  f"{quote_sheetname(md.title)}!{absolute_coordinate('E2:E3')}"
+defn = DefinedName("H_OBJTYPE", attr_text=ref)
+wb.defined_names.add(defn)
+
+
+add_enums_table(table_name='TABLE6',
+                column_header='I_OBJTYPE',
+                column_index=6,
+                enum_values= ['FSTN',
+                              'LSTN'
+                              ],
+                ws_meta=md)
+
+ref =  f"{quote_sheetname(md.title)}!{absolute_coordinate('F2:F4')}"
+defn = DefinedName("I_OBJTYPE", attr_text=ref)
+wb.defined_names.add(defn)
+
+
+ws.cell(row=1, column=3).value = 'Category'
+dv = DataValidation(type="list", formula1='category_range', allow_blank=True)
+ws.add_data_validation(dv)
+dv.add('C2:C1000')
+
+ws.cell(row=1, column=4).value = 'ObjType'
+dv = DataValidation(type="list", formula1='INDIRECT(C2 & "_OBJTYPE")', allow_blank=True)
+ws.add_data_validation(dv)
+dv.add('D2:D1000')
+
+wb.save("pumsmo2.xlsx")
+
+print(f"{quote_sheetname('metadata_tables')}")
+print(f"{absolute_coordinate('A1:A5')}")
+
+
